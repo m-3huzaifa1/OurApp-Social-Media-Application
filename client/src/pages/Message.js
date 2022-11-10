770db9428e3d0a0b710bb5adecbe4699a9172676
Mohd Huzaifa <mohdhuzaifa80@gmail.com>
2022-11-10 12:52:10 +0530

    OurApp

--- client/src/pages/Message.js
+++ client/src/pages/Message.js
@@ -7,12 +7,14 @@ export default function Message() {
         <DefaultLayout>
             <Row justify="center">
                 <Col lg={12} xs={24} >
+                <div className="bs1 msg m-5 p-3 text-center">
                     <br/><br/>
-                    <h1>Chat using ChatExpress</h1>
+                    <h1>Chat using Chat Express</h1>
                     <br/><br/>
-                    <h3>Official Messaging product of <a>OurApp</a> <br/>Social Platform</h3>
+                    <h3>Official Messaging Application of <a>OurApp</a> <br/>Social Platform</h3>
                     <br/><br/>
-                    <Button style={{color:'white',backgroundColor:'blue',paddingBottom:'50px',paddingTop:'10px',paddingLeft:'35px',paddingRight:'35px',border:'1px solid blue',borderRadius:'15px',height:'50px',fontSize:'25px'}}><a href="https://chat-expresss.netlify.app">{'--> '}   Go to ChatExpress</a></Button>
+                    <Button style={{color:'white',backgroundColor:'blue',paddingBottom:'30px',paddingTop:'10px',paddingLeft:'15px',paddingRight:'15px',border:'1px solid blue',borderRadius:'15px',height:'25px',fontSize:'13px'}}><a href="https://chat-expresss.netlify.app">{'--> '}   Go to ChatExpress</a></Button>
+                </div>
                 </Col>
                </Row>
         </DefaultLayout>
