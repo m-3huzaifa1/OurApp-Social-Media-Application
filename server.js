197074b565768b4cebdf3e6fe1fb069a419982de
Mohd Huzaifa <mohdhuzaifa80@gmail.com>
2023-01-01 13:47:21 +0530

    server

--- server.js
+++ server.js
@@ -29,11 +29,11 @@ app.use('/api/posts', postRoute);
 
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV == 'production')
+/*if (process.env.NODE_ENV == 'production')
 {
   app.use('/',express.static('client/build'))
   app.get("*",(req,res)=>{
     res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
   });
-}
+}*/
 app.listen(port,()=> console.log(`Server is running on port ${port}`));
